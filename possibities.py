537064470854839013f01042e6c34f8cc27449b1
jan-Kulpas <krzychu3813@gmail.com>
2022-06-03 20:02:47 +0200

    trying to speed up

--- possibities.py
+++ possibities.py
@@ -1,17 +1,21 @@
 from main import check_conditions
 from main import load_allowed_guesses
 from main import table2color
+import multiprocessing as mp
 
 import math
+import time
+from operator import itemgetter
 from itertools import product
+from itertools import repeat
+from scipy.stats import entropy
+
+all_lists = list(product([3,1,0],repeat=5));
+all_colors = [table2color(x) for x in all_lists]
 
 #Zwraca liste słów które dalej są możliwe
 def reduce(guess, colors, words):
-    space = [];
-    for word in words:
-        if fits_rules(guess, colors, word):
-            space.append(word)
-    return space
+    return [word for word in words if fits_rules(guess,colors,word)]
 
 #Sprawdza czy słowo jest legalne po danej próbie
 def fits_rules(guess,colors,word):
@@ -20,21 +24,25 @@ def fits_rules(guess,colors,word):
 
 #Liczy entropie słowa -- WIP
 def get_entropy(guess,words):
-    all_lists = list(product([3,1,0],repeat=5));
-    all_colors = [table2color(x) for x in all_lists]
-    sum = 0
+    list = []
     for color in all_colors:
         new_words = reduce(guess,color,words)
         prob = len(new_words)/len(words)
-        if (prob == 0):
-            continue
-        bits = math.log2(1/prob)
-        sum += prob * bits
-    return sum
+        list.append(prob)
+    sum = entropy(list, base=2)
+    print("|= " + guess + " : " + str(sum))
+    return (guess, sum)
 
+def find_best_guess(words):
+    entropies = []
+    with mp.Pool(mp.cpu_count()) as pool:
+        entropies = pool.starmap(get_entropy, zip(words, repeat(words)))
 
+    best = max(entropies, key=itemgetter(1))
 
+    return best
 
-xd = load_allowed_guesses()
-f = get_entropy("crane",xd)
-print(f)
+words = load_allowed_guesses()
+words = words[:1000]
+(guess, e) = find_best_guess(words)
+print(guess + " : " + str(e))
