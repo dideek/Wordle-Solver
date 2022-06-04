81682cfe80543b30c9f05b28fafdf00cece693c5
jan-Kulpas <krzychu3813@gmail.com>
2022-06-04 18:54:13 +0200

    Live play option added

---
+++ wordle_guide.py
@@ -0,0 +1,40 @@
+from main import *
+
+def string2color(s):
+    colors = 0
+    guide = {"g":3,"y":1,"b":0}
+    for letter in s:
+        colors += guide[letter]
+        colors*=4
+    colors//=4
+
+    return colors
+
+def play_guide():
+    global words, first_guess
+    if not first_guess:
+        first_guess = find_best_guess(words)[0]
+
+    score = 0
+    new_words = words.copy()
+
+    while score < 6:
+        guess = find_best_guess(new_words)[0] if score > 0 else first_guess
+        print(f"Powinieneś strzelić \"{guess}\"")
+
+        print("Podaj kolory:")
+        input_color = input()
+        colors = string2color(input_color)
+        score += 1
+        print_colored_input(guess, colors2table(colors))
+        if check_win_condition(colors):
+            print()
+            return score
+        new_words.remove(guess)
+        new_words = reduce(guess, colors, new_words)
+        print(f" [{len(new_words)}] words left")
+
+if __name__ == '__main__':
+    first_guess = "raise"
+    words = load_words()
+    play_guide()
